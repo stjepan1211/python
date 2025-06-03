0ad2b739374787d41557de8533c9717096aa4462
stjepan <stjepanbaricevic1211@gmail.com>
2025-06-03 15:01:16 -0400

    add argumets

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = "eth0" 
-new_mac = "00:11:22:33:44:55"
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its mac address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New mac address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 print (f"[+] changing MAC address for {interface} to {new_mac}")
 
 #don't use string concat because of command injection
